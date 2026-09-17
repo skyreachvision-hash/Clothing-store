70573e87b782f71a8520d2327d3ae04241c99f2e
skyreachvision-hash <skyreach.vision@gmail.com>
2026-09-17 19:29:02 +0200

    Restore categories Pages Function proxy to Worker backend

--- functions/api/categories.js
+++ functions/api/categories.js
@@ -1,6 +1,17 @@
-import originalWorker from "../../src/index.js";
-import { handleCategoryApi } from "../../src/category-api.js";
-
 export async function onRequest(context) {
-  return handleCategoryApi(context.request, context.env, originalWorker);
+  const { request, env } = context;
+  const targetUrl = new URL(request.url);
+  targetUrl.protocol = "https:";
+  targetUrl.hostname = "clothing-store-api";
+
+  const init = {
+    method: request.method,
+    headers: new Headers(request.headers)
+  };
+
+  if (request.method !== "GET" && request.method !== "HEAD") {
+    init.body = request.body;
+  }
+
+  return env.API.fetch(new Request(targetUrl.toString(), init));
 }
