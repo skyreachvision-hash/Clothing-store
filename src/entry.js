c6ae40d674af6b359cd84b732f670eae79bb464e
skyreachvision-hash <skyreach.vision@gmail.com>
2026-09-19 12:56:57 +0200

    Route shipping API through Worker entry

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,12 @@
 import originalWorker from "./index.js";
 import { handleCategoryApi } from "./category-api.js";
+import { handleShippingApi } from "./shipping-api.js";
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     if (url.pathname === "/api/categories") return handleCategoryApi(request, env, originalWorker);
+    if (url.pathname === "/api/shipping") return handleShippingApi(request, env, originalWorker);
     return originalWorker.fetch(request, env, ctx);
   }
 };
