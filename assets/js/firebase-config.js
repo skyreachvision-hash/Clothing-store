b653cab7889f5bb0c23ba4e006734b7d1d79d2be
skyreachvision-hash <skyreach.vision@gmail.com>
2026-09-19 13:58:32 +0200

    Share Firebase configuration across admin and customer auth

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQjotIJvYeEggt-zn5wlvRSoMcVcgPgaU",
+  authDomain: "clothing-store-e7200.firebaseapp.com",
+  projectId: "clothing-store-e7200",
+  storageBucket: "clothing-store-e7200.firebasestorage.app",
+  messagingSenderId: "631043708400",
+  appId: "1:631043708400:web:43b15745d1a4b7c41f7935",
+  measurementId: "G-XEZ10CHQHB"
+};
