5d72fd73ed165802b16c7245b1b64625fb632331
skyreachvision-hash <skyreach.vision@gmail.com>
2026-09-19 13:58:59 +0200

    Use shared Firebase configuration

--- assets/js/customer-session.js
+++ assets/js/customer-session.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { firebaseConfig } from "./firebase-config.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDQjotIJvYeEggt-zn5wlvRSoMcvCgPgaU",
-  authDomain: "clothing-store-e7200.firebaseapp.com",
-  projectId: "clothing-store-e7200",
-  storageBucket: "clothing-store-e7200.firebasestorage.app",
-  messagingSenderId: "631043708400",
-  appId: "1:631043708400:web:43b15745d1a4b7c41f7935",
-  measurementId: "G-XEZ10CHQHB"
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
